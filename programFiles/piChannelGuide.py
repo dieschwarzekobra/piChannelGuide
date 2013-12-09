de923817a4fc91ee3406f38823d55e7c8434a23b
Jaleesa Powell <jaleesap@email.unc.edu>
2013-12-09 15:47:48 -0500

    Design the program. #7

---
+++ programFiles/piChannelGuide.py
@@ -0,0 +1,18 @@
+#Import libraries
+#Declare global variables
+#Define global functions
+#Define program-level functions
+#Clear the cache
+#Connect to TVRage Full Schedule. Download the schedule for parsing.
+#Define channels that are important to the user.
+#Print this list of channels to the display and save it to the cache.
+#Parse the data provided by the user to separate the channels from the channel numbers.
+#Create a list of the channels present in the TV schedule.
+#Compare the user-provided list of channels to the list of channels present in the TV schedule. Compose a list of the channels each list has in common.
+#Get local time.
+#Convert all times to Universal Time, rounded to the nearest hour.
+#Parse TVRage schedule and restructure 'show' element to include a channel and time attribute.
+#Select upcoming shows that are showing within an hour and a half of the current local time.
+#Print those listings and save them to the cache.
+#Display the cached data in the webbrowser
+#Repeat every thirty minutes.
